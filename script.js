8045f13ad163bcd1f836e27c9c5146d6b026db0e
TOP <e227599@sbl.ch>
2022-11-16 14:52:18 +0100

    Red green change

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function handleKeyPress(ev) {
-    console.log(ev)
-
+    if (ev.code === "space") {
+        const player = document.querySelector("#player")
+        player.setAttribute("cx", 100*Math.random())
+        player.setAttribute("cy", 100*Math.random())
+    } else if (ev.key === "r") {
+        player.settAttribute("fill", "red")
+    } else if (ev.key === "g") {
+        player.settAttribute("fill", "green")
+    }
 }
-
 window.onkeydown = handleKeyPress
